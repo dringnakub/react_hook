1c4ef678a4a4a7b61d3371d4c6740e32f6f97f67
Komson Kawichai <komsan.kk1992@gmail.com>
2020-03-26 23:25:38 +0700

    fetch data

--- readme.txt
+++ readme.txt
@@ -6,3 +6,29 @@ vscode extension
 5. path intell
 6. matching tag
 7. prettier
+
+  // let var1 = 0;
+  // const var2 = "hello";
+  // const var3 = true;
+  // const [count, setCount] = useState(0)
+
+
+
+      <h1>Drink Rider {var1}</h1>
+      <h1>Drink Rider {var2}</h1>
+      <h1>Drink Rider {var3 && "Yes"}</h1>
+      {var3 && <h1>YES</h1>}
+      {var3 ? <h1>Yesss</h1> : <h1>No</h1>}
+
+      <h2>Count: {count}</h2>
+      <button onClick={() => {
+        var1 = var1 + 1;
+        setCount(count + 1);
+        console.log("var1 ", var1)
+      }}>Add</button>
+
+      <CounterClassComponent />
+      <CounterFunctionalComponent />
+
+      <Ex1Style />
+      <Ex2Image />
--- src/App.js
+++ src/App.js
@@ -1,35 +1,55 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import CounterClassComponent from './CounterClassComponent';
 import CounterFunctionalComponent from './CounterFunctionalComponent';
 import Ex1Style from './components/ex1_styles';
 import Ex2Image from './components/ex2_image';
+import Axios from 'axios';
 
 function App() {
-  let var1 = 0;
-  const var2 = "hello";
-  const var3 = true;
-  const [count, setCount] = useState(0)
+
+  const [data, setData] = useState({ users: [] });
+
+
+  const fetchUsers = async () => {
+    console.log("fetchUsers");
+    const mArray = await Axios.get('https://jsonplaceholder.typicode.com/users');
+    setData({
+      users: mArray.data
+    });
+  }
+
+  const showUsers = () => {
+     return data && data.users.map(uu => (
+     <li key={uu.id} >name : {uu.name} username : {uu.username}</li>
+     ))
+  }
+  //   useEffect(() => {
+  //     (async function() {
+  //         try {
+  //             const response = await fetch(
+  //                 'https://jsonplaceholder.typicode.com/users'
+  //             );
+  //             const json = await response.json();
+  //             setData(json.data);
+  //         } catch (e) {
+  //             console.error(e);
+  //         }
+  //     })();
+  // }, []);
+
+  useEffect(() => {
+    console.log("useEffect");
+    fetchUsers();
+
+  }, [])
+
+
   return (
     <div>
-      <h1>Drink Rider {var1}</h1>
-      <h1>Drink Rider {var2}</h1>
-      <h1>Drink Rider {var3 && "Yes"}</h1>
-      {var3 && <h1>YES</h1>}
-      {var3 ? <h1>Yesss</h1> : <h1>No</h1>}
-
-      <h2>Count: {count}</h2>
-      <button onClick={() => {
-        var1 = var1 + 1;
-        setCount(count + 1);
-        console.log("var1 ", var1)
-      }}>Add</button>
-
-      <CounterClassComponent />
-      <CounterFunctionalComponent />
-
-      <Ex1Style />
-      <Ex2Image />
+      <h1>https://jsonplaceholder.typicode.com/users</h1>
+      <ul>{showUsers()}</ul>
     </div>
+
   )
 }
 
